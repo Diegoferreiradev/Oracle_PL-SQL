b1804da04ae122eef3001f3cdcf8f1e491f01126
Diegoferreiradev <diegoferreiradev@gmail.com>
2020-02-16 22:48:32 -0300

    Adição de Records

--- Capítulo 17/tipos_de_dados_record_e_collection.sql
+++ Capítulo 17/tipos_de_dados_record_e_collection.sql
@@ -17,3 +17,63 @@ BEGIN
   DBMS_OUTPUT.put_line('Cidade: ' || registro.cidade);
 END;  
 
+
+-- Exemplo 2
+
+SELECT * FROM TALUNO;
+
+DECLARE
+   reg TALUNO%ROWTYPE;
+   vCep VARCHAR2(10) := '98000000';
+BEGIN
+  SELECT COD_ALUNO, NOME, CIDADE
+  INTO reg.cod_aluno, reg.nome, reg.cidade
+  FROM TALUNO
+  WHERE COD_ALUNO = 1;
+  
+  
+  reg.Cep := '99000897';
+  reg.Salario := 5533.22;
+  
+  DBMS_OUTPUT.put_line('Código: ' || reg.cod_aluno);
+  DBMS_OUTPUT.put_line('Nome: ' || reg.nome);
+  DBMS_OUTPUT.put_line('Cidade: ' || reg.cidade);
+  DBMS_OUTPUT.put_line('CEP: ' || reg.cep);
+  DBMS_OUTPUT.put_line('Salário: ' || reg.salario);
+END;   
+   
+   
+-- EXEMPLO 3
+
+DECLARE
+   TYPE T_ALUNO IS TABLE OF TALUNO.NOME%TYPE
+   INDEX BY BINARY_INTEGER;
+   
+   registro T_ALUNO;
+BEGIN
+  registro(1) := 'Diego Ferreira';
+  registro(2) := 'Joana Lima';
+  registro(3) := 'Fernanda Nascimento';
+  
+  DBMS_OUTPUT.put_line('Nome 1: ' || registro(1));
+  DBMS_OUTPUT.put_line('Nome 2: ' || registro(2));
+  DBMS_OUTPUT.put_line('Nome 3: ' || registro(3));
+
+END;      
+   
+   
+-- EXEMPLO 4
+
+DECLARE
+   TYPE nome_type IS TABLE OF TALUNO.NOME%TYPE;
+   nome_table nome_type := nome_type();
+BEGIN
+  nome_table.EXTEND;
+  nome_table(1) := 'Diego Ferreira';
+  nome_table.EXTEND;
+  nome_table(2) := 'Fernanda Nirá';
+  
+  DBMS_OUTPUT.put_line('Nome: ' || nome_table(1));
+  DBMS_OUTPUT.put_line('Nome: ' || nome_table(2));
+END;      
+
